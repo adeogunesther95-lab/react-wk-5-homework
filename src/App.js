51482a9595f329703cbd190bb05f8c5ef9b0836a
adeogunesther95-lab <adeogunesther95@gmail.com>
2026-03-25 05:33:02 +0100

    footer added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      Weather Application
+      <footer>
+        This project was coded by
+        <a
+          href="https://github.com/adeogunesther95-lab"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Esther Adeogun
+        </a>
+        open-sourced on
+        <a
+          href="https://github.com/adeogunesther95-lab/vanilla-weather-week8"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        and hosted on
+        <a
+          href="https://vanilla-weather-week8.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
