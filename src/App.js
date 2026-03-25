306011d224453de1732dbc87654791d2b8fcf185
adeogunesther95-lab <adeogunesther95@gmail.com>
2026-03-25 06:09:47 +0100

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather Application</h1>
-      <footer>
-        This project was coded by
-        <a
-          href="https://github.com/adeogunesther95-lab"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Esther Adeogun
-        </a>
-        open-sourced on
-        <a
-          href="https://github.com/adeogunesther95-lab/react-wk-5-homework"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-        and hosted on
-        <a
-          href="https://ea-react-weather-app.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather Application</h1>
+        <footer>
+          This project was coded by
+          <a
+            href="https://github.com/adeogunesther95-lab"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Esther Adeogun
+          </a>
+          open-sourced on
+          <a
+            href="https://github.com/adeogunesther95-lab/react-wk-5-homework"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          and hosted on
+          <a
+            href="https://ea-react-weather-app.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
